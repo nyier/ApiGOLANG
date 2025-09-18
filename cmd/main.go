010ed8a08e7fee426a74361f7c487382335d307a
Nyıerre <108092383+nyier@users.noreply.github.com>
2025-09-17 22:55:19 -0600

    added html to it

--- cmd/main.go
+++ cmd/main.go
@@ -5,28 +5,34 @@ import (
 	"net/http"
 )
 
+/*
+	func main() {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello from Kubernetes with Go!")
+		})
+		http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "{\"status\": \"ok\"}")
+		})
+
+		fmt.Println("Starting server on :8080")
+		http.ListenAndServe(":8080", nil)
+	}
+*/
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello from Kubernetes with Go!")
-	})
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "{\"status\": \"ok\"}")
+		html := `<html>
+		<head>
+			<title>Isaac Rodriguez</title>
+		</head>
+		<body>
+
+
+			<h1>Hello from Kubernetes with Go!</h1>
+		</body>
+		</html>`
+		fmt.Fprintf(w, html)
 	})
 
 	fmt.Println("Starting server on :8080")
 	http.ListenAndServe(":8080", nil)
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	html := `<html>
-	<head>
-		<title>Isaac Rodriguez</title>
-	</head>
-	<body>
-
-
-		<h1>Hello from Kubernetes with Go!</h1>
-	</body>
-	</html>`
-	fmt.Fprintf(w, html)
 }
