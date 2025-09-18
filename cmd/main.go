9c65f8a3eb80ae219a8ab5e26d7a46f960252c35
Nyıerre <108092383+nyier@users.noreply.github.com>
2025-09-17 20:22:16 -0600

    Update
    Added and reorganized files for a standard GO project.

--- cmd/main.go
+++ cmd/main.go
@@ -16,3 +16,17 @@ func main() {
 	fmt.Println("Starting server on :8080")
 	http.ListenAndServe(":8080", nil)
 }
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	html := `<html>
+	<head>
+		<title>Isaac Rodriguez</title>
+	</head>
+	<body>
+
+
+		<h1>Hello from Kubernetes with Go!</h1>
+	</body>
+	</html>`
+	fmt.Fprintf(w, html)
+}
